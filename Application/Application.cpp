b9cc04baebf345360d832bd67a184711dfe8f832
Andreyko <Andreyko@ANDY>
2023-10-01 21:50:23 +0700

    Добавьте файлы проекта.

---
+++ Application/Application.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const double PI = 3.1415;
+    double b = PI;
+
+    if (0 > 10)
+        cout << "quieqhfhqwfjjk;fqfj";
+    else
+    {
+        cout << "\n123123\n";
+    }
+        
+    int a = 1;
+    switch (a)
+    {
+    case 1:
+        cout << "1";
+        int a3 = 1;
+        cout << "a3 = " << a3;
+    case 2:
+        cout << "2";
+    default:
+        cout << "error";
+        break;
+    }
+
+
+    cout << "end";
+    return 0;
+}
